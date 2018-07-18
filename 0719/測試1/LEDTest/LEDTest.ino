2ce03931aa9ba11167387883a60cae2d7b46d247
Pakesw <40046034+Pakesw@users.noreply.github.com>
2018-07-19 03:04:39 +0800

    Add files via upload

---
+++ 0719/測試1/LEDTest/LEDTest.ino
@@ -0,0 +1,16 @@
+#include "FPS_GT511C3.h"
+#include "SoftwareSerial.h"
+FPS_GT511C3 fps(4, 5);
+void setup() {
+  Serial.begin(9600);
+  fps.UseSerialDebug = true;
+  fps.Open();
+}
+
+void loop() {
+  fps.SetLED(true); // turn on the LED inside the fps
+  while(1)
+  {
+  delay(1000);
+  }
+}
